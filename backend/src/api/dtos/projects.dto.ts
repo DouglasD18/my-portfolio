e9a54d5bda279cfebac3a653a2ef76bd50282e9d
Douglinhas <daguiaralcantara@gmail.com>
2023-01-17 19:27:01 -0300

    Crinado a conexão com o banco, crinado um .env e criando o ProjectsDTO.

---
+++ backend/src/api/dtos/projects.dto.ts
@@ -0,0 +1,49 @@
+import { IsArray, IsMongoId, IsNotEmpty, IsString } from 'class-validator';
+
+export class ProjectsDTO {
+  @IsMongoId({
+    message: "Id must be a MongoDB ObjectId."
+  })
+  id: string;
+
+  @IsNotEmpty({
+    message: "Title should not be empty."
+  })
+  @IsString({
+    message: "Title must be a string."
+  })
+  title: string;
+
+  @IsNotEmpty({
+    message: "Description should not be empty."
+  })
+  @IsString({
+    message: "Description must be a string."
+  })
+  description: string;
+
+  @IsNotEmpty({
+    message: "Image should not be empty."
+  })
+  @IsString({
+    message: "Image must be a string."
+  })
+  image: string;
+
+  @IsString({
+    groups: ["backend", "frontend", "fullstack"],
+    message: "Must have a valid category!"
+  })
+  @IsNotEmpty({
+    message: "Category should not be empty."
+  })
+  category: "backend" | "frontend" | "fullstack";
+
+  @IsNotEmpty({
+    message: "Tecnologies should not be empty."
+  })
+  @IsArray({
+    message: "Tecnologies must be a array of strings."
+  })
+  tecnologies: string[];
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from 'cors';
+import 'dotenv/config';
+
+const PORT = Number(process.env.PORT);
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000, () => console.log("Is running!"));
+app.listen(PORT, () => console.log("Is running!", PORT));

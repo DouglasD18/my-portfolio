62407b9450b4bb9fbfff37ba631bc41284c94e4b
Douglinhas <daguiaralcantara@gmail.com>
2023-01-17 23:03:46 -0300

    Implemetação da entidade Project.

---
+++ backend/src/api/entities/Project.ts
@@ -0,0 +1,16 @@
+enum Category {
+  fullstack = "fullstack",
+  back = "backend",
+  front = "frontend"
+}
+
+export class Project {
+  constructor(
+    private title: string,
+    private url: string,
+    private description: string,
+    private image: string,
+    private category: Category,
+    private tecnologies: string
+  ) {}
+}

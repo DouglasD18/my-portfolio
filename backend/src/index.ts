c2727f33dd51067d4e84f60ff488fbba4ee8db1b
Douglinhas <daguiaralcantara@gmail.com>
2023-01-26 14:58:46 -0300

    Criação da documentação parcial da API com Swagger.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from 'cors';
 import 'dotenv/config';
+
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
+
 import { InsertProjectsFactory } from './api/factories/InsertProjects.factory';
 import { GetProjectsFactory } from "./api/factories/GetProjects.factory";
 import { JWTAuth } from "./utils/auth/JWT.auth";
@@ -12,16 +16,16 @@ app.use(express.json());
 app.use(cors());
 
 const authorization = JWTAuth;
-
 const insertProject = InsertProjectsFactory.make();
 const getProjects = GetProjectsFactory.make();
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.post(
   "/project",
   (req, res, next) => authorization.verifyMiddleware(req, res, next),
   (req, res, next) => insertProject.insert(req, res, next)
 );
-
 app.get("/projects", (req, res, next) => getProjects.read(req, res, next));
 
 app.listen(PORT, () => console.log("Is running!", PORT));

0caa4c7429cc6fea0fa131f062a3b351ff30e1bf
Douglinhas <daguiaralcantara@gmail.com>
2023-01-27 18:57:29 -0300

    Criação de seção about.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import About from "./sessions/About";
 import Header from "./sessions/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <About />
+      </main>
     </>
   )
 }
---
+++ frontend/src/sessions/About.tsx
@@ -0,0 +1,14 @@
+import "../styles/about.css";
+
+function About() {
+  return (
+    <section>
+      <div  className="about">
+        <h2>Olá, meu nome é Douglas Aguiar e eu sou Desenvolvedor Web.</h2>
+        <p>Sou um cara que demorou um pouco para perceber o que queria estudar, trabalhar e viver para o resto da vida, mas encontrou a programação e se encontrou no ato. Hoje, sou apaixonado por programação e todo o mundo de possibilidades que ela me proporciona.</p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
